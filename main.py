7a47b93d900e716e70082acfed39d0b4abd8fda6
hydrusbeta <hydrusgamma@gmail.com>
2023-08-19 18:43:50 -0600

    Somehow forgot to update the name of the command-line interface module. Fixed.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ ARCHITECTURE_NAME = 'controllable_talknet'
 ARCHITECTURE_ROOT = os.path.join(ROOT_DIR, ARCHITECTURE_NAME)
 RESULTS_DIR = os.path.join(ARCHITECTURE_ROOT, 'results')
 PYTHON_EXECUTABLE = os.path.join(ROOT_DIR, '.venvs', 'controllable_talknet', 'bin', 'python')
-INFERENCE_CODE_PATH = os.path.join(ARCHITECTURE_ROOT, 'controllable_talknet_cli.py')
+INFERENCE_CODE_PATH = os.path.join(ARCHITECTURE_ROOT, 'command_line_interface.py')
 
 TALKNET_INPUT_FORMAT, TALKNET_INPUT_EXTENSION = 'WAV', '.wav'
 REFERENCE_PREFIX = 'Reference_'
