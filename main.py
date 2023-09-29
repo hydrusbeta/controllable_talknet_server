45df48ea8f38ee49773d59f6430d75749311031e
hydrusbeta <hydrusgamma@gmail.com>
2023-09-28 22:10:19 -0600

    It is now possible to specify a cache implementation via command line. controllable_talknet_server also now expects a GPU ID and Session ID to be passed in the payload from Hay Say and will use the specified GPU.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import base64
 import json
 import os
@@ -9,6 +10,7 @@ import jsonschema
 import soundfile
 from flask import Flask, request
 from hay_say_common import *
+from hay_say_common.cache import Stage
 from jsonschema.exceptions import ValidationError
 
 ARCHITECTURE_NAME = 'controllable_talknet'
@@ -23,142 +25,148 @@ REFERENCE_PREFIX = 'Reference_'
 app = Flask(__name__)
 
 
-@app.route('/generate', methods=['POST'])
-def generate() -> (str, int):
-    code = 200
-    message = ""
-    try:
-        user_text, input_filename_sans_extension, disable_reference_audio, character, pitch_factor, auto_tune, \
-            reduce_metallic_sound, output_filename_sans_extension = parse_inputs()
-        pitch_options = assemble_pitch_options(auto_tune, reduce_metallic_sound)
-        reference_audio = prepare_reference_audio(input_filename_sans_extension, disable_reference_audio)
-        link_model_path(character)
-        execute_program(user_text, reference_audio, character, pitch_factor, pitch_options)
-        copy_output_audio(get_singleton_file(RESULTS_DIR), output_filename_sans_extension)
-        clean_up(get_temp_files())
-    except BadInputException:
-        code = 400
-        message = traceback.format_exc()
-    except Exception:
-        code = 500
-        message = construct_full_error_message(ARCHITECTURE_ROOT, get_temp_files())
+def register_methods(cache):
+    @app.route('/generate', methods=['POST'])
+    def generate() -> (str, int):
+        code = 200
+        message = ""
+        try:
+            user_text, input_filename_sans_extension, disable_reference_audio, character, pitch_factor, auto_tune, \
+                reduce_metallic_sound, output_filename_sans_extension, gpu_id, session_id = parse_inputs()
+            pitch_options = assemble_pitch_options(auto_tune, reduce_metallic_sound)
+            reference_audio = prepare_reference_audio(input_filename_sans_extension, disable_reference_audio, cache,
+                                                      session_id)
+            link_model_path(character)
+            execute_program(user_text, reference_audio, character, pitch_factor, pitch_options, gpu_id)
+            copy_output_audio(get_singleton_file(RESULTS_DIR), output_filename_sans_extension, cache, session_id)
+            clean_up(get_temp_files())
+        except BadInputException:
+            code = 400
+            message = traceback.format_exc()
+        except Exception:
+            code = 500
+            message = construct_full_error_message(ARCHITECTURE_ROOT, get_temp_files())
 
-    # The message may contain quotes and curly brackets which break JSON syntax, so base64-encode the message.
-    message = base64.b64encode(bytes(message, 'utf-8')).decode('utf-8')
-    response = {
-        "message": message
-    }
+        # The message may contain quotes and curly brackets which break JSON syntax, so base64-encode the message.
+        message = base64.b64encode(bytes(message, 'utf-8')).decode('utf-8')
+        response = {
+            "message": message
+        }
 
-    return json.dumps(response, sort_keys=True, indent=4), code
+        return json.dumps(response, sort_keys=True, indent=4), code
 
-
-def parse_inputs():
-    schema = {
-        'type': 'object',
-        'properties': {
-            'Inputs': {
-                'type': 'object',
-                'properties': {
-                    'User Text': {'type': 'string'},
-                    'User Audio': {'type': 'string'}
+    def parse_inputs():
+        schema = {
+            'type': 'object',
+            'properties': {
+                'Inputs': {
+                    'type': 'object',
+                    'properties': {
+                        'User Text': {'type': 'string'},
+                        'User Audio': {'type': 'string'}
+                    },
+                    'required': ['User Text']
                 },
-                'additionalProperties': False,
-                'required': ['User Text']
-            },
-            'Options': {
-                'type': 'object',
-                'properties': {
-                    'Architecture': {'type': 'string'},
-                    'Disable Reference Audio': {'type': 'boolean'},
-                    'Character': {'type': 'string'},
-                    'Pitch Factor': {'type': 'integer'},
-                    'Auto Tune': {'type': 'boolean'},
-                    'Reduce Metallic Sound': {'type': 'boolean'}
+                'Options': {
+                    'type': 'object',
+                    'properties': {
+                        'Architecture': {'type': 'string'},
+                        'Disable Reference Audio': {'type': 'boolean'},
+                        'Character': {'type': 'string'},
+                        'Pitch Factor': {'type': 'integer'},
+                        'Auto Tune': {'type': 'boolean'},
+                        'Reduce Metallic Sound': {'type': 'boolean'}
+                    },
+                    'required': ['Disable Reference Audio', 'Character', 'Pitch Factor', 'Auto Tune', 'Reduce Metallic Sound']
                 },
-                'additionalProperties': False,
-                'required': ['Disable Reference Audio', 'Character', 'Pitch Factor', 'Auto Tune', 'Reduce Metallic Sound']
+                'Output File': {'type': 'string'},
+                'GPU ID': {'type': ['string', 'integer']},
+                'Session ID': {'type': ['string', 'null']}
             },
-            'Output File': {'type': 'string'}
-        },
-        'additionalProperties': False,
-        'required': ['Inputs', 'Options', 'Output File']
-    }
+            'required': ['Inputs', 'Options', 'Output File', 'GPU ID', 'Session ID']
+        }
 
-    try:
-        jsonschema.validate(instance=request.json, schema=schema)
-    except ValidationError as e:
-        raise BadInputException(e.Message)
+        try:
+            jsonschema.validate(instance=request.json, schema=schema)
+        except ValidationError as e:
+            raise BadInputException(e.Message)
 
-    user_text = request.json['Inputs']['User Text']
-    input_filename_sans_extension = request.json['Inputs']['User Audio']
-    disable_reference_audio = request.json['Options']['Disable Reference Audio']
-    character = request.json['Options']['Character']
-    pitch_factor = request.json['Options']['Pitch Factor']
-    auto_tune = request.json['Options']['Auto Tune']
-    reduce_metallic_sound = request.json['Options']['Reduce Metallic Sound']
-    output_filename_sans_extension = request.json['Output File']
+        user_text = request.json['Inputs']['User Text']
+        input_filename_sans_extension = request.json['Inputs']['User Audio']
+        disable_reference_audio = request.json['Options']['Disable Reference Audio']
+        character = request.json['Options']['Character']
+        pitch_factor = request.json['Options']['Pitch Factor']
+        auto_tune = request.json['Options']['Auto Tune']
+        reduce_metallic_sound = request.json['Options']['Reduce Metallic Sound']
+        output_filename_sans_extension = request.json['Output File']
+        gpu_id = request.json['GPU ID']
+        session_id = request.json['Session ID']
 
-    return user_text, input_filename_sans_extension, disable_reference_audio, character, pitch_factor,auto_tune, \
-        reduce_metallic_sound, output_filename_sans_extension
+        return user_text, input_filename_sans_extension, disable_reference_audio, character, pitch_factor,auto_tune, \
+            reduce_metallic_sound, output_filename_sans_extension, gpu_id, session_id
+
+    def assemble_pitch_options(auto_tune, reduce_metallic_sound):
+        return [
+            *(('pc',) if auto_tune else ()),
+            *(('srec',) if reduce_metallic_sound else ()),
+        ]
+
+    class BadInputException(Exception):
+        pass
+
+    def link_model_path(character):
+        """Create a symbolic link to the model folder in the location where Controllable TalkNet expects to find it."""
+        character_dir = get_model_path(ARCHITECTURE_NAME, character)
+        symlink_dir = os.path.join(ARCHITECTURE_ROOT, 'models', character)
+        create_link(character_dir, symlink_dir)
+
+    def prepare_reference_audio(input_filename_sans_extension, disable_reference_audio, cache, session_id):
+        """Temporarily copy the input file to the location where Controllable Talknet expects to find it."""
+        if disable_reference_audio or input_filename_sans_extension is None:
+            return None
+        target = os.path.join(ARCHITECTURE_ROOT, REFERENCE_PREFIX + input_filename_sans_extension + TALKNET_INPUT_EXTENSION)
+        try:
+            array, samplerate = cache.read_audio_from_cache(Stage.PREPROCESSED, session_id, input_filename_sans_extension)
+            soundfile.write(target, array, samplerate, format=TALKNET_INPUT_FORMAT)
+        except Exception as e:
+            raise Exception("Unable to copy file from Hay Say's audio cache to controllable_talknet's root directory.") \
+                from e
+        return target
+
+    def execute_program(user_text, reference_audio, character, pitch_factor, pitch_options, gpu_id):
+        arguments = [
+            '--text', user_text,
+            '--custom_model', character,
+            *(('--reference_audio', reference_audio) if reference_audio else ()),
+            *(('--pitch_factor', str(pitch_factor)) if pitch_factor else ()),
+            *(('--pitch_options', *pitch_options) if pitch_options else ())
+        ]
+        env = select_hardware(gpu_id)
+        subprocess.run([PYTHON_EXECUTABLE, INFERENCE_CODE_PATH, *arguments], env=env)
+
+    def get_temp_files():
+        reference_audios = get_files_with_extension(ARCHITECTURE_ROOT, TALKNET_INPUT_EXTENSION)
+        output_files = [os.path.join(RESULTS_DIR, file) for file in os.listdir(RESULTS_DIR)]
+        return reference_audios + output_files
+
+    def copy_output_audio(input_path, output_filename_sans_extension, cache, session_id):
+        """Copy the output file to the location where Hay Say expects to find it."""
+        try:
+            array, samplerate = read_audio(input_path)
+            cache.save_audio_to_cache(Stage.OUTPUT, session_id, output_filename_sans_extension, array, samplerate)
+        except Exception as e:
+            raise Exception("Unable to copy file from Hay Say's audio cache to controllable_talknet's root directory.") \
+                from e
 
 
-def assemble_pitch_options(auto_tune, reduce_metallic_sound):
-    return [
-        *(('pc',) if auto_tune else ()),
-        *(('srec',) if reduce_metallic_sound else ()),
-    ]
-
-class BadInputException(Exception):
-    pass
-
-
-def link_model_path(character):
-    """Create a symbolic link to the model folder in the location where Controllable TalkNet expects to find it."""
-    character_dir = get_model_path(ARCHITECTURE_NAME, character)
-    symlink_dir = os.path.join(ARCHITECTURE_ROOT, 'models', character)
-    create_link(character_dir, symlink_dir)
-
-
-def prepare_reference_audio(input_filename_sans_extension, disable_reference_audio):
-    """Temporarily copy the input file to the location where Controllable Talknet expects to find it."""
-    if disable_reference_audio or input_filename_sans_extension is None:
-        return None
-    target = os.path.join(ARCHITECTURE_ROOT, REFERENCE_PREFIX + input_filename_sans_extension + TALKNET_INPUT_EXTENSION)
-    try:
-        array, samplerate = read_audio_from_cache(PREPROCESSED_DIR, input_filename_sans_extension)
-        soundfile.write(target, array, samplerate, format=TALKNET_INPUT_FORMAT)
-    except Exception as e:
-        raise Exception("Unable to copy file from Hay Say's audio cache to controllable_talknet's root directory.") \
-            from e
-    return target
-
-
-def execute_program(user_text, reference_audio, character, pitch_factor, pitch_options):
-    arguments = [
-        '--text', user_text,
-        '--custom_model', character,
-        *(('--reference_audio', reference_audio) if reference_audio else ()),
-        *(('--pitch_factor', str(pitch_factor)) if pitch_factor else ()),
-        *(('--pitch_options', *pitch_options) if pitch_options else ())
-    ]
-    subprocess.run([PYTHON_EXECUTABLE, INFERENCE_CODE_PATH, *arguments])
-
-
-def get_temp_files():
-    reference_audios = get_files_with_extension(ARCHITECTURE_ROOT, TALKNET_INPUT_EXTENSION)
-    output_files = [os.path.join(RESULTS_DIR, file) for file in os.listdir(RESULTS_DIR)]
-    return reference_audios + output_files
-
-
-def copy_output_audio(input_path, output_filename_sans_extension):
-    """Copy the output file to the location where Hay Say expects to find it."""
-    try:
-        array, samplerate = read_audio(input_path)
-        save_audio_to_cache(OUTPUT_DIR, output_filename_sans_extension, array, samplerate)
-    except Exception as e:
-        raise Exception("Unable to copy file from Hay Say's audio cache to controllable_talknet's root directory.") \
-            from e
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog='main.py', description='A webservice interface for TTS inference with Controllable Talknet')
+    parser.add_argument('--cache_implementation', default='file', choices=cache_implementation_map.keys(), help='Selects an implementation for the audio cache, e.g. saving them to files or to a database.')
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
+    args = parse_arguments()
+    cache = select_cache_implementation(args.cache_implementation)
+    register_methods()
     app.run(debug=True, host='0.0.0.0', port=6574)
